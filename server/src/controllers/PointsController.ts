38ae90fbd48163ad4d8fd0a35606ade706504d64
Marcio Costa <marcioc424@gmail.com>
2020-06-04 21:45:53 -0300

    Test gitignore

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -4,27 +4,35 @@ import knex from '../database/connection';
 class PointsController {
 
     async index(request : Request, response : Response) {
-        const { city, uf, items } = request.query;
+        const { city, uf, items, name } = request.query;
 
         const parsedItems = String(items)
             .split(',')
             .map(item => Number(item.trim()));
-        
+            
         const points = await knex('points')
             .join('point_items', 'points.id', '=', 'point_items.point_id')
-            .whereIn('point_items.item_id', parsedItems)
-            .where('city', String(city))
-            .where('uf', String(uf))
+            .where('city', 'like', '%' + String(city) + '%')
             .distinct()
+            .modify(builder => {
+                if(items !== ''){
+                    builder.whereIn('point_items.item_id', parsedItems)
+                }
+                if(uf !== '')
+                    builder.where('uf', String(uf))
+                if(name !== '')
+                    builder.where('name', String(name))
+            })
             .select('points.*');
-
         return response.json(points);
     }
 
     async show(request : Request, response : Response) {
-        const { id } = request.params;
+        const { id, name } = request.params;
 
-        const point = await knex('points').where('id', id).first();
+        const point = await knex('points')
+            .where('id', id)
+            .first();
 
         if(!point){
             return response.status(400).json({ message: 'Point not found.' });
